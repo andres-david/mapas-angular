814634809b2bd35fb77ed9a870f1ad5ca864a238
Andrés Cedeño <andresdalopez92@gmail.com>
2022-09-08 08:21:12 +0200

    first commit

---
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -0,0 +1,158 @@
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+interface MarcadorColor {
+  color  : string;
+  marker?: mapboxgl.Marker;
+  centro?: [number, number];
+}
+
+@Component({
+  selector: 'app-marcadores',
+  templateUrl: './marcadores.component.html',
+  styles: [
+`
+  .mapa-container {
+        width: 100%;
+        height: 100%;
+      }
+
+  .list-group {
+    position: fixed;
+    top: 20px;
+    right: 20px;
+    z-index: 99;
+  }
+
+  li{
+    cursor: pointer;
+  }
+`
+
+  ]
+})
+export class MarcadoresComponent implements AfterViewInit {
+
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number, number] = [-3.6840935915493587, 40.41401549710406];
+
+  //Arreglo de marcadores
+
+  marcadores: MarcadorColor[] = [];
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel,
+    });
+
+    this.leerLocalStorage();
+
+  }
+
+  agregarMarcador(){
+
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+
+    const nuevoMarcador = new mapboxgl.Marker({
+      draggable: true,
+      color: color
+    })
+      .setLngLat(this.center)
+      .addTo(this.mapa);
+
+    this.marcadores.push({
+      color,
+      marker: nuevoMarcador
+    });
+
+    this.guardarMarcadoresLocalStorage();
+
+    nuevoMarcador.on('dragend', () => {
+
+      this.guardarMarcadoresLocalStorage();
+
+    })
+
+  }
+
+  irMarcador( marcador: mapboxgl.Marker ){
+
+    this.mapa.flyTo({
+      center: marcador.getLngLat(),
+    })
+
+  }
+
+  guardarMarcadoresLocalStorage ( ) {
+
+    const lngLatArr: MarcadorColor[] = []; 
+
+    this.marcadores.forEach( m => {
+
+      const color = m.color;
+      const {lng, lat} = m.marker!.getLngLat();
+
+      lngLatArr.push({
+        color: color,
+        centro: [lng, lat],
+      });
+    })
+
+    localStorage.setItem('marcadores', JSON.stringify(lngLatArr));
+
+  }
+
+
+  leerLocalStorage ( ) {
+
+    if( !localStorage.getItem('marcadores') ){
+      return;
+    }
+
+    const lngLatArr : MarcadorColor[] = JSON.parse( localStorage.getItem('marcadores')! );
+
+    lngLatArr.forEach( m => {
+
+      const newMarker = new mapboxgl.Marker({
+        color: m.color,
+        draggable: true,
+      })
+      .setLngLat( m.centro! )
+      .addTo( this.mapa );
+
+      this.marcadores.push({
+        marker: newMarker,
+        color: m.color,
+      });
+
+      newMarker.on('dragend', () => {
+
+        this.guardarMarcadoresLocalStorage();
+
+      })
+
+
+    });
+
+  }
+
+  borrarMarcador( i : number){
+
+    this.marcadores[i].marker?.remove();
+
+    this.marcadores.splice(i, 1);
+
+    this.guardarMarcadoresLocalStorage();
+
+  }
+
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiYW5kcmVzLTI5IiwiYSI6ImNsMjJ6enZiMDA0NzQzanFrN3Z4c3RxejQifQ.dlmE5vkLBhkHDfIokDpSPA'
+};
